bc0c43ed91836daaa651c640164853e775f79f1c
T300Y <tedzone9@gmail.com>
2022-07-13 19:15:43 +1000

    updated comparision function: manipulated user input based on criteria

--- project.py
+++ project.py
@@ -57,37 +57,33 @@ def main():
         database(inputs, input('Time/Distance for comparision:'))
 
 def comparision(wr, time, event):
-    print(event)
     field_events = [
     "Men's Long Jump", "Men's High Jump", "Men's Shot Put", "Men's Javelin","Men's Discus Throw", "Men's Triple Jump",
     "Women's Long Jump", "Women's High Jump", "Women's Shot Put", "Women's Javelin","Women's Discus Throw", "Women's Triple Jump"
 ]
     track_events = [
-        "Men's 100 m", "Men's 200 m", "Men's 400 m", "Men's 800 m", "Men's 1500 m", "Men's 5000 m"
+        "Men's 100 m", "Men's 200 m", "Men's 400 m", "Men's 800 m", "Men's 1500 m", "Men's 5000 m",
         "Women's 100 m", "Women's 200 m", "Women's 400 m", "Women's 800 m", "Women's 1500 m", "Women's 5000 m"
     ]
     if time.strip() == '':
         return 'N/A'
     elif event in track_events:
-        print('check1')
         wr = wr.split(':')
         mins = int(wr[0]) * 60
         if ':' in time:
-            mins1, seconds = time.split(':')
-            mins1 = int(mins1) * 60
+            min, time = time.split(':')
+            mins1 = int(min) * 60
         else:
             mins1 = 0
-        percentage = (mins+float(wr[1])) / (mins1+float(seconds))* 100
+        percentage = (mins+float(wr[1])) / (mins1+float(time))* 100
         percentage = round(percentage, 2)
         percentage =f"{percentage}%"
-        print(percentage)
         return percentage
 
     elif event in field_events:
         percentage = float(time) / float(wr)* 100
         percentage = round(percentage, 2)
         percentage =f"{percentage}%"
-        print(percentage)
         return percentage
 
     elif event in ["Men's 10000 m", "Women's 10000 m"]:
@@ -101,11 +97,8 @@ def comparision(wr, time, event):
         percentage = (mins+float(wr[2])) / (mins1+float(seconds))* 100
         percentage = round(percentage, 2)
         percentage =f"{percentage}%"
-        print(percentage)
         return percentage
 
-    else:
-        print('test failed')
 
 
 
@@ -139,7 +132,7 @@ def database(inputs, time):
                             'FIRST':x[6].value,
                             'LAST':x[5].value,
                             'CLASSIFICATION':classification_col,
-                            'TIME':x[9].value,
+                            'TIME/DISTANCE':x[9].value,
                             'NPC':x[7].value,
                             'BIRTH':x[8].value,
                             'WIND':x[10].value,
